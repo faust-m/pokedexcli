e6583692fad0f0ccbae7f1116c2f790228bb687b
Mike Faust <github.upstairs688@slmails.com>
2024-12-19 22:54:20 -0500

    Updated main.go and internals

--- main.go
+++ main.go
@@ -3,40 +3,27 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/url"
 	"os"
 	"strings"
+	//"github.com/faust-m/pokedexcli/internal/pokeapi"
 )
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	cmds := initCommands()
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			if text := scanner.Text(); len(text) > 0 {
-				value, ok := cmds[cleanInput(text)[0]]
-				if !ok {
-					fmt.Println("Unknown command")
-					continue
-				}
-				value.callback()
-			}
-		}
-	}
+type config struct {
+	next     url.URL
+	previous url.URL
 }
 
-func cleanInput(text string) []string {
-	return strings.Fields(strings.ToLower(text))
-}
+var cmds map[string]cliCommand
 
-func initCommands() map[string]cliCommand {
-	cmds := map[string]cliCommand{
+func init() {
+	cmds = map[string]cliCommand{
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
@@ -47,11 +34,41 @@ func initCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the names of the next 20 location areas",
+			callback:    commandMap,
+		},
 	}
-	return cmds
 }
 
-func commandExit() error {
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			if text := scanner.Text(); len(text) > 0 {
+				value, ok := cmds[cleanInput(text)[0]]
+				if !ok {
+					fmt.Println("Unknown command")
+					continue
+				}
+				switch value.name {
+				case "map":
+					value.callback(&config{})
+				default:
+					value.callback(&config{})
+				}
+			}
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit(*config) error {
 	if _, err := fmt.Println("Closing the Pokedex... Goodbye!"); err != nil {
 		return fmt.Errorf("error in commandExit: %w", err)
 	}
@@ -59,7 +76,7 @@ func commandExit() error {
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(*config) error {
 	heading :=
 		`Welcome to the Pokedex!
 Usage:
@@ -68,11 +85,14 @@ Usage:
 	if _, err := fmt.Print(heading); err != nil {
 		return fmt.Errorf("error printing heading in commandHelp: %w", err)
 	}
-	cmds := initCommands()
 	for _, v := range cmds {
-		if _, err := fmt.Printf("%s: %s\n", v.name, v.description); err != nil {
+		if _, err := fmt.Printf("%s:\t%s\n", v.name, v.description); err != nil {
 			return fmt.Errorf("error printing command in commandHelp: %w", err)
 		}
 	}
+	return nil
+}
+
+func commandMap(cfg *config) error {
 	return nil
 }
